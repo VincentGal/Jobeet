cec8e85282230870c7fb9bb54047b6382611d622
VincentGal <vincent.gal@expertplug.com>
2012-09-12 18:22:00 +0200

    after day 13 finish

--- symfony.dev/jobeet/apps/frontend/modules/job/templates/showSuccess.php
+++ symfony.dev/jobeet/apps/frontend/modules/job/templates/showSuccess.php
@@ -2,6 +2,11 @@
 <?php use_helper('Text') ?>
  
 <div id="job">
+  
+  <?php if ($sf_request->getParameter('token') == $job->getToken()): ?>
+        <?php include_partial('job/admin', array('job' => $job)) ?>
+  <?php endif ?>
+    
   <?php slot(
   'title',
   sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition())) ?>
@@ -15,8 +20,7 @@
   <?php if ($job->getLogo()): ?>
     <div class="logo">
       <a href="<?php echo $job->getUrl() ?>">
-        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
-          alt="<?php echo $job->getCompany() ?> logo" />
+        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>" alt="<?php echo $job->getCompany() ?> logo" />
       </a>
     </div>
   <?php endif ?>
@@ -32,10 +36,5 @@
   <div class="meta">
     <small>posted on <?php echo $job->getDateTimeObject('created_at')->format('m/d/Y') ?></small>
   </div>
- 
-  <div style="padding: 20px 0">
-    <a href="<?php echo url_for('job/edit?id='.$job->getId()) ?>">
-      Edit
-    </a>
-  </div>
+
 </div>
